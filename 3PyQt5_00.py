35e262dd2c23adc2fdf029ee35fd6c58e1c6752e
jjypainter <jypainter@naver.com>
2020-09-12 22:06:10 +0900

    Hello MainWindow! Hello World Again!

---
+++ 3PyQt5_00.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 21:56:09 2020
+
+@author: ganglim
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        
+        self.setWindowTitle("Hello! MainWindow!")
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+        
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
